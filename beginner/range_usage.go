2a420d4bd68821b97518c32780179205d1c0cac6
Girish.D <girishdondapati10@gmail.com>
2018-08-26 21:28:17 +0530

    Summary of Range
    Iterates over elements in a variety of data structuresusing range

---
+++ beginner/range_usage.go
@@ -0,0 +1,37 @@
+// _range_ iterates over elements in a variety of data
+// structures. Let's see how to use `range` with some
+// of the data structures we've already learned.
+
+package main
+
+import "fmt"
+
+func main() {
+    nums := []int{2, 3, 4}
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    fmt.Println("sum:", sum)
+
+    for i, num := range nums {
+        if num == 3 {
+            fmt.Println("index:", i)
+        }
+    }
+
+    // `range` on map iterates over key/value pairs.
+    kvs := map[string]string{"a": "apple", "b": "banana"}
+    for k, v := range kvs {
+        fmt.Printf("%s -> %s\n", k, v)
+    }
+
+    // `range` can also iterate over just the keys of a map.
+    for k := range kvs {
+        fmt.Println("key:", k)
+    }
+
+    for i, c := range "go" {
+        fmt.Println(i, c)
+    }
+}
